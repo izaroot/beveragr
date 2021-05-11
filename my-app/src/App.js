8607b8c662383c28990e71376feb2872ab9b1938
Isaac <izaroot327@gmail.com>
2021-05-11 15:47:23 -0400

    Completed user login/register functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,10 +26,40 @@ export default class App extends Component{
     })
   }
 
+  handleLogin = (event, username) => {
+      event.preventDefault()
+      console.log(username)
+      fetch(`http://localhost:3000/users?username=${username}`)
+      .then(resp => resp.json())
+      .then(userArr =>{
+        if(userArr.length <= 0){
+          alert('This user does not exist.')
+        }else{
+          let user = userArr[0]
+          console.log(user)
+          this.setState({
+            user
+          })
+        }
+      })
+  }
+
+  handeNewUserLogin = (user) => {
+    this.setState({
+      user
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      user: {}
+    })
+  }
+
   render(){
     return(
       <Router>
-        <Navbar user={this.state.user} />
+        <Navbar user={this.state.user} login={this.handleLogin} logout={this.handleLogout} newUser={this.handeNewUserLogin} />
         <Switch>
           <Route path="/createbev">
             <CreateBeverage />
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react'
-import {Segment, Label, Form} from 'semantic-ui-react'
+import {Segment, Label, Form, Button} from 'semantic-ui-react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -11,29 +11,79 @@ export default class Navbar extends Component{
 
     state = {
         loginDisplay: false,
-        registerDisplay: false
+        registerDisplay: false,
+        login: "",
+        register: "",
+        registerName: ""
     }
 
+    handleLoginDisplay = () => {
+        this.setState({
+            loginDisplay: true
+        })
+    }
 
+    handleRegisterDisplay = () => {
+        this.setState({
+            registerDisplay: true
+        })
+    }
+
+    handleBackDisplay = () => {
+        this.setState({
+            loginDisplay: false,
+            registerDisplay: false,
+            login: "",
+            register: ""
+        })
+    }
+
+    handleFormChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleRegister = () => {
+        fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                username: this.state.register,
+                drinks: []
+            })
+        })
+        .then(resp => resp.json())
+        .then(newUser => {
+            this.props.newUser(newUser)
+        })
+    }
 
     render(){
         let welcome
-        if (this.props.user.name){
-            welcome = <Label as='a' image>{this.props.user.name}</Label>
+        if (this.props.user.username){
+            welcome = <Fragment>
+                        <Label as='a' image>{this.props.user.username}</Label>
+                        <Button onClick={() => {this.props.logout(); this.handleBackDisplay();}}>Logout</Button>
+                    </Fragment>
         }else{
             if (!this.state.loginDisplay && !this.state.registerDisplay){
-                welcome = (<Fragment><button>Login</button> <button>Register</button></Fragment>)
+                welcome = (<Fragment><button onClick={this.handleLoginDisplay}>Login</button> <button onClick={this.handleRegisterDisplay}>Register</button></Fragment>)
             }else if (this.state.loginDisplay){
                 welcome =
-                <Form>
-                    <input type="text" name='login' placeholder='Username' />
+                <Form onChange={(e) => this.handleFormChange(e)} onSubmit={(e) => this.props.login(e, this.state.login)} >
+                    <input type="text" name='login' value={this.state.login} placeholder='Username' />
                     <input type="submit" value='Login' />
+                    <button onClick={this.handleBackDisplay}>Back</button>
                 </Form>
             } else if (this.state.registerDisplay){
                 welcome =                 
-                <Form>
-                    <input type="text" name='register'placeholder='New User' />
+                <Form onChange={(e) => this.handleFormChange(e)} onSubmit={(e) => this.handleRegister(e)} >
+                    <input type="text" name='register' value={this.state.register} placeholder='New User' />
                     <input type="submit" value='Register' />
+                    <button onClick={this.handleBackDisplay}>Back</button>
                 </Form>
             }
         }
