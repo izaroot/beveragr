272901bdc5d590f51ca6c098e7d74b3bd7260fcc
Isaac <izaroot327@gmail.com>
2022-02-08 14:23:19 -0500

    Added gh pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css'
+import { HashRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
- <App />,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById('root')
 );
 
